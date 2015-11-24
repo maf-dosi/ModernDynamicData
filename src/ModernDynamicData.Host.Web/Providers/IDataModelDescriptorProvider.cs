43ea37dc33e92f4f46549aadc52b6b34c1e5be24
ORMES jean-Michel <jormes@hotmail.fr>
2015-11-24 11:48:35 +0100

    Merge devs to web project v.1.0 : - add reference to Abstractions project - add infrastructure, controllers, providers and dependency injection.

---
+++ src/ModernDynamicData.Host.Web/Providers/IDataModelDescriptorProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ModernDynamicData.Abstractions.DataProviders;
+
+namespace ModernDynamicData.Host.Web.Providers
+{
+    public interface IDataModelDescriptorProvider
+    {
+        int GetNumberOfDataModelDescriptors();
+        IEnumerable<DataModelDescriptor> GetAllDataModelDescriptors();
+        DataModelDescriptor GetDataModelDescriptorByName(string name);
+        void AddDataModelDescriptor(DataModelDescriptor dataModelDescriptor);
+    }
+}
