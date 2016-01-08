e9c450094e7e18b63a7d6ef5e3967bc4202a7964
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2016-01-08 13:04:08 +0100

    Clean code

--- samples/ModernDynamicData.Samples.Host.Web/Startup.cs
+++ samples/ModernDynamicData.Samples.Host.Web/Startup.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.FileProviders;
-using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Framework.DependencyInjection;
 using ModernDynamicData.Samples.Host.Web.DataProvider;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNet.Mvc.Razor;
-using ModernDynamicData.Infrastructure;
 
 namespace ModernDynamicData.Samples.Host.Web
 {
@@ -15,17 +11,13 @@ namespace ModernDynamicData.Samples.Host.Web
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDynamicData(new PhysicalFileProvider(
-                    @"C:\Users\mgrosperrin\Documents\GitHub\maf-dosi\ModernDynamicData\src\ModernDynamicData"));
+            services.AddDynamicData();
         }
 
-        public void Configure(IApplicationBuilder app, IServiceProvider serviceProvider, IHostingEnvironment hostingEnvironment)
+        public void Configure(IApplicationBuilder app, IServiceProvider serviceProvider)
         {
             // Add the platform handler to the request pipeline.
             app.UseIISPlatformHandler();
-
-            hostingEnvironment.WebRootFileProvider = new PhysicalFileProvider(
-                @"C:\Users\mgrosperrin\Documents\GitHub\maf-dosi\ModernDynamicData\src\ModernDynamicData\wwwroot");
 
             app.RunDynamicData(serviceProvider, new FakeDataModelDescriptor("Test"),
                 new FakeDataModelDescriptor("Test2"));
